1c2992cdefabe99449ae41b5da996af23b9c61f9
lucianmat <1640858+lucianmat@users.noreply.github.com>
2023-08-23 18:27:36 +0300

    added docker-composer app publish fixed kestrel certificate error

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,8 +1,23 @@
 using Asp.Versioning.ApiExplorer;
 using DrawService.Entities;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(o =>
+{ 
+    o.ConfigureHttpsDefaults(opts =>
+    {   
+        opts.ClientCertificateMode = ClientCertificateMode.AllowCertificate;
+        opts.AllowAnyClientCertificate();
+        opts.ClientCertificateValidation = (cert, chain, policyErrors) =>
+        {
+            // Certificate validation logic here
+            // Return true if the certificate is valid or false if it is invalid
+            return true;
+        };
+    });
+});
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddProblemDetails();
@@ -23,26 +38,25 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+} 
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
+
+var descriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+app.UseSwaggerUI(c=>{
+    c.RoutePrefix = string.Empty; // default request rerouted to "swagger"
+
+    foreach (var description in descriptionProvider.ApiVersionDescriptions)
+    {
+        c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", 
+            description.GroupName.ToUpperInvariant());
+    }
     
-    // Enable middleware to serve generated Swagger as a JSON endpoint.
-    app.UseSwagger();
-
-    var descriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-   
-    app.UseSwaggerUI(c=>{
-        c.RoutePrefix = string.Empty; // default request rerouted to "swagger"
-
-        foreach (var description in descriptionProvider.ApiVersionDescriptions)
-        {
-            c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", 
-                description.GroupName.ToUpperInvariant());
-        }
-       
-    });
-
-}
+});
 
 app.UseHttpsRedirection();
+app.UseHsts();
 
 app.UseAuthorization();
 app.UseHttpLogging(); 
